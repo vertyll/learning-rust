b4ea56093e90a4f8a5853ed4f82b2e773bff0230
Mikołaj Gawron <gawrmiko@gmail.com>
2024-03-30 13:52:18 +0100

    add match expression

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,7 +1,42 @@
-#![allow(unused)]
-fn main() {
-    enum Option<T> {
-        None,
-        Some(T),
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("Ćwiartka ze stanu {:?}!", state);
+            25
+        }
     }
+}
+
+fn main() {
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("{:?}", six);
+    println!("{:?}", none);
 }
