25d03d15fa66754d7e757605f0ea3b9b0165ef6c
Mikołaj Gawron <gawrmiko@gmail.com>
2024-05-03 12:31:39 +0200

    feat: improve integration tests

--- helion_zaawansowane/echo-clap/tests/cli.rs
+++ helion_zaawansowane/echo-clap/tests/cli.rs
@@ -10,5 +10,54 @@ fn pass_no_args() -> TestResult {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("Usage:"));
+
+    Ok(())
+}
+
+#[test]
+fn check_new_line() -> TestResult {
+    // let expected = fs::read_to_string("tests/expected/new_line")?;
+    // let mut cmd = Command::cargo_bin("echo-clap")?;
+    //
+    // cmd.args(&["Mikołaj", "Dom", "Pałac"])
+    //     .assert()
+    //     .success()
+    //     .stdout(expected);
+    //
+    // Ok(())
+
+    let args = &["Mikołaj", "Dom", "Pałac"];
+    let file_path = "tests/expected/new_line";
+
+    run_test(args, file_path)
+}
+
+#[test]
+fn check_no_new_line() -> TestResult {
+    // let expected = fs::read_to_string("tests/expected/no_new_line")?;
+    // let mut cmd = Command::cargo_bin("echo-clap")?;
+    //
+    // cmd.args(&["-n", "Mikołaj", "Dom", "Pałac"])
+    //     .assert()
+    //     .success()
+    //     .stdout(expected);
+    //
+    // Ok(())
+
+    let args = &["-n", "Mikołaj", "Dom", "Pałac"];
+    let file_path = "tests/expected/no_new_line";
+
+    run_test(args, file_path)
+}
+
+fn run_test(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    let mut cmd = Command::cargo_bin("echo-clap")?;
+
+    cmd.args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+
     Ok(())
 }
